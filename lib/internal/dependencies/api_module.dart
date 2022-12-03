76a372e65ded206628f0e7ffd00ce6f6e9825c66
Spison <sanya.kanash@yandex.ru>
2022-12-03 12:25:47 +0300

    Using IP, not  win...

--- lib/internal/dependencies/api_module.dart
+++ lib/internal/dependencies/api_module.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_study/data/clients/auth_client.dart';
 
-String baseUrl = "http://win-rbj5ik80shi:1000/";
+String baseUrl = "http://192.168.0.104:1000/";
 
 class ApiModule {
   static AuthClient? _authClient;
